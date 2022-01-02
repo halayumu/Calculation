d4d20c4269b01147b7c0d33c58a4f033de3cfc0d
halayumu <ookamiayumuwww@gmail.com>
2022-01-03 01:48:44 +0900

    テキスト情報の演算関数作成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 new Vue({
     el: "#app",
+
+    //----[htmlのテキスト情報]----//
     data: {
-        int: "",
+        seven: "", //一万円
+        six: "", //五千円
+        five: "", //千円
+        four: "", //五百円
+        three: "", //百円
+        two: "", //五十円
+        one: "" //十円
     },
+
+    //----[演算処理]----//
+    computed: {
+        sevenAnswer: function() { return this.seven * 10000 }, //一万の計算
+        sixAnswer: function() { return this.six * 5000 }, //五千円の計算
+        fiveAnswer: function() { return this.five * 1000 }, //千円の計算
+        fourAnswer: function() { return this.four * 500 }, //五百円の計算
+        threeAnswer: function() { return this.three * 100 }, //百円の計算
+        twoAnswer: function() { return this.two * 50 }, //五十円の計算
+        oneAnswer: function() { return this.one * 10 }, //五十円の計算
+    }
 })
