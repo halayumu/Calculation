35b6070fabb117787e9ec17f2a687b1b9608dfb9
halayumu <ookamiayumuwww@gmail.com>
2022-02-11 01:28:29 +0900

    jsエラー処理削除

--- js/main.js
+++ js/main.js
@@ -18,60 +18,25 @@ new Vue({
     //----[演算処理]----//
     computed: {
         sevenAnswer: function() { //一万の計算
-            if (isNaN(this.seven)) {
-                alert('数値を入力してください');
-                return 0
-            } else {
-                return this.seven * 10000
-            }
+            return this.seven * 10000
         },
         sixAnswer: function() { //五千円の計算
-            if (isNaN(this.six)) {
-                alert('数値を入力してください');
-                return 0
-            } else {
-                return this.six * 5000
-            }
+            return this.six * 5000
         },
         fiveAnswer: function() { //千円の計算
-            if (isNaN(this.five)) {
-                alert('数値を入力してください');
-                return 0
-            } else {
-                return this.five * 1000
-            }
+            return this.five * 1000
         },
         fourAnswer: function() { //五百円の計算
-            if (isNaN(this.four)) {
-                alert('数値を入力してください');
-                return 0
-            } else {
-                return this.four * 500
-            }
+            return this.four * 500
         },
         threeAnswer: function() { //百円の計算
-            if (isNaN(this.three)) {
-                alert('数値を入力してください');
-                return 0
-            } else {
-                return this.three * 100
-            }
+            return this.three * 100
         },
         twoAnswer: function() { //五十円の計算
-            if (isNaN(this.two)) {
-                alert('数値を入力してください');
-                return 0
-            } else {
-                return this.two * 50
-            }
+            return this.two * 50
         },
-        oneAnswer: function() {
-            if (isNaN(this.one)) { //五十円の計算
-                alert('数値を入力してください');
-                return 0
-            } else {
-                return this.one * 10
-            }
+        oneAnswer: function() { //十円の計算
+            return this.one * 10
         },
 
         //----[合計処理]----//
